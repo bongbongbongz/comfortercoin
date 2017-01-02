49f48511dfaff10f86bfb858f855dccf8637f3a1
xolani_masuku <mcxolani@gmail.com>
2017-01-02 13:01:58 +0200

    add table responsive

--- src/pages/components/User.js
+++ src/pages/components/User.js
@@ -15,30 +15,32 @@ class User extends Component {
 					<div className="panel-body">
 						<input type="text" className="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
 					</div>
-					<table className="table table-hover">
-                        <tbody>
-                            <tr>
-                                <th>address</th>
-                                <th>bitcoinWallet</th>
-                                <th>Country</th>
-                                <th>email</th>
-                                <th>fullName</th>
-                                <th>number</th>
-                                <th>postcode</th>
-                            </tr>
-                            {this.props.users.map(user=>{
-                                return (<tr key={user.number}>
-                                    <td>{user.address}</td>
-                                    <td>{user.bitcoinWallet}</td>
-                                    <td>{user.country}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.fullName}</td>
-                                    <td>{user.number}</td>
-                                    <td>{user.postcode}</td>
-                                </tr>);
-                            })}
-                        </tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table className="table table-hover">
+                            <tbody>
+                                <tr>
+                                    <th>address</th>
+                                    <th>bitcoinWallet</th>
+                                    <th>Country</th>
+                                    <th>email</th>
+                                    <th>fullName</th>
+                                    <th>number</th>
+                                    <th>postcode</th>
+                                </tr>
+                                {this.props.users.map(user=>{
+                                    return (<tr key={user.number}>
+                                        <td>{user.address}</td>
+                                        <td>{user.bitcoinWallet}</td>
+                                        <td>{user.country}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.fullName}</td>
+                                        <td>{user.number}</td>
+                                        <td>{user.postcode}</td>
+                                    </tr>);
+                                })}
+                            </tbody>
+                        </table>
+				    </div>
 				</div>
 			</div>
            </div>
